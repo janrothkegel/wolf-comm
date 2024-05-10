d7675359649a8f5768f9b40093c104766c8b1d98
mtielen <6302356+mtielen@users.noreply.github.com>
2024-05-11 01:22:31 +0200

    Update version number to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wolf_comm',
-    version='0.0.7',
+    version='0.0.8',
     author="Jan Rothkegel",
     author_email="jan.rothkegel@web.de",
     description="A package to communicate with Wolf SmartSet Cloud",
