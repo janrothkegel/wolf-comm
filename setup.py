f63c0f70673e70359bda6fbc087c5e87bf6d90b2
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-09-19 21:15:03 +0200

    Added way of translating entities Added GroupName before entity's name in order to remove duplicate problem.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wolf_comm',
-    version='0.0.9',
+    version='0.0.10',
     author="Jan Rothkegel",
     author_email="jan.rothkegel@web.de",
     description="A package to communicate with Wolf SmartSet Cloud",
